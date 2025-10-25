3599488140e537dd4bf2cfd6c5d23fc3c4d22394
Leticia-SS <leticia.saraiva013@gmail.com>
2025-10-25 20:26:26 -0300

    optimize function cause numba doesnt use random or print or time libraries

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,30 +1,78 @@
-import random
 import time
 import math
+from numba import njit, prange
+import numpy as np
 
 n = 10000000
 
 def monte_carlo_puro():
-    inicio = time.time()
     dentroCircunferencia = 0
 
     for _ in range(n):
-        x = random.random()
-        y = random.random()
+        x = np.random.random()
+        y = np.random.random()
 
         if x**2 + y**2 <= 1:
             dentroCircunferencia += 1
 
-    piEstimado = 4 * dentroCircunferencia/n
-    final = time.time()
+    return 4 * dentroCircunferencia/n
+
+@njit()
+def monte_carlo_numba():
+    dentroCircunferencia = 0
+
+    for _ in prange(n):
+        x = np.random.random()
+        y = np.random.random()
+
+        if x ** 2 + y ** 2 <= 1:
+            dentroCircunferencia += 1
+
+    return 4 * dentroCircunferencia / n
+
+@njit(cache=True)
+def monte_carlo_numba_cache():
+    dentroCircunferencia = 0
+
+    for _ in prange(n):
+        x = np.random.random()
+        y = np.random.random()
+
+        if x ** 2 + y ** 2 <= 1:
+            dentroCircunferencia += 1
+
+    return  4 * dentroCircunferencia / n
+
+def calculos(piEstimado):
     erroAbsoluto = math.fabs(math.pi - piEstimado)
-    erroPercentual = ((math.pi-piEstimado)/math.pi)*100
+    erroPercentual = ((math.pi - piEstimado) / math.pi) * 100
 
     print(f"Valor n: {n}")
-    print(f"π: {math.pi} \nπ~: {piEstimado:.4f} \nTempo: {final-inicio:.6f} s")
+    print(f"π: {math.pi} \nπ Estimado: {piEstimado:.4f}")
     print(f"Erro absoluto: {erroAbsoluto:.4f}")
     print(f"Erro percentual: {erroPercentual:.4f}")
 
-monte_carlo_puro()
+
+print("Puro")
+inicio = time.time()
+puro = monte_carlo_puro()
+final = time.time()
+print(f"Tempo: {final - inicio:.6f} s")
+calculos(puro)
+
+print("Otimizado")
+monte_carlo_numba()
+inicio = time.time()
+numba = monte_carlo_numba()
+final = time.time()
+print(f"Tempo: {final - inicio:.6f} s")
+calculos(numba)
 
 
+print("Otimizado com Cache")
+monte_carlo_numba_cache()
+inicio = time.time()
+cache = monte_carlo_numba_cache()
+final = time.time()
+print(f"Tempo: {final - inicio:.6f} s")
+calculos(cache)
